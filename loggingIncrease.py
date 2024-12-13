eea4cad42c55e87fec91fd881cb48338ae12e66d
adrianazizsantoso22 <adrian22pass@gmail.com>
2024-12-14 05:52:33 +0700

    Update loggingIncrease.py

--- loggingIncrease.py
+++ loggingIncrease.py
@@ -4,4 +4,9 @@ import logging
 logging.basicConfig(filename='server.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def log_event(event):
+    """Fungsi ini mencatat event ke dalam file log dengan format yang telah ditentukan.
+    
+    Args:
+        event (str): Pesan yang akan dicatat ke dalam log.
+    """
     logging.info(event)
