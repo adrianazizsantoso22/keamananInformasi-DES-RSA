2653d41874b20b45b70337e01f903de89141f8f0
adrianazizsantoso22 <adrian22pass@gmail.com>
2024-12-15 15:41:16 +0700

    Update rsa.tes.py

--- rsa.tes.py
+++ rsa.tes.py
@@ -1,25 +1,18 @@
 from rsa import RSA
 
 def main():
-    """Fungsi utama untuk menguji fungsionalitas RSA.
-    
-    Menghasilkan kunci RSA, mengenkripsi, dan mendekripsi pesan
-    untuk memastikan bahwa semuanya berfungsi dengan baik.
-    """
     rsa = RSA()
-
     rsa.generate_rsa_keys()
-
+    
     print("Public Key:", rsa.public_key)
     print("Private Key:", rsa.private_key)
 
     message = "Hello, World!"
-
     encrypted_message = rsa.encrypt(message, rsa.public_key)
     print("Encrypted Message:", encrypted_message)
 
     decrypted_message = rsa.decrypt(encrypted_message)
     print("Decrypted Message:", decrypted_message)
-    
+
 if __name__ == "__main__":
     main()
